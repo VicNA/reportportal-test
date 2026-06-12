c549a8df3e1ee72fc8718fd74c399a7f1df84f24
viktor <vnikolaeff989@gmail.com>
2026-06-12 12:28:52 +0300

    fix: перенос ObjectMapper в конфигурацию клиента api

--- src/main/java/ru/effectivemobile/api/client/ApiClient.java
+++ src/main/java/ru/effectivemobile/api/client/ApiClient.java
@@ -1,9 +1,30 @@
 package ru.effectivemobile.api.client;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.restassured.RestAssured;
+import io.restassured.config.ObjectMapperConfig;
 import io.restassured.specification.RequestSpecification;
 import ru.effectivemobile.api.specs.RequestSpecifications;
+import ru.effectivemobile.config.ApiConfig;
+import ru.effectivemobile.config.AppConfig;
 
 public abstract class ApiClient {
 
-    protected RequestSpecification requestSpec = RequestSpecifications.defaultSpec();
+    protected final ApiConfig api = AppConfig.api();
+
+    protected RequestSpecification requestSpec = RequestSpecifications.defaultSpec(api);
+
+    static {
+        ObjectMapper jacksonMapper = new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        RestAssured.config = RestAssured.config()
+                .objectMapperConfig(
+                        ObjectMapperConfig.objectMapperConfig()
+                                .jackson2ObjectMapperFactory(
+                                        (cls, charset) -> jacksonMapper));
+    }
 }
